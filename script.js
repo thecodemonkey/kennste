3cba2f6f14a519823229bd8088486aaa6da3bc26
thecodemonkey <7ilya@gmx.de>
2021-04-25 18:15:19 +0200

    basic stuff ready

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('hello!')
+console.log("hello!");
+
+var heads = [0, 1, 2, 3, 4, 5];
+
+function getCtx() {
+  var c = document.getElementById("cnvs");
+  return c.getContext("2d");
+}
+
+function drawPixel(ctx, l, r) {
+  ctx.fillRect(l, r, 10, 10);
+}
+
+function drawHeads(heads) {
+  var ctx = getCtx();
+
+  for (var x = 0; x < 1000; x++) {
+    drawPixel(ctx, 10 + x * 10, 10);
+  }
+}
+
+function update() {
+  drawHeads(heads);
+}
