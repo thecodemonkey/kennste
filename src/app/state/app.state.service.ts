3d076aa1fd0d3849705a47d8cb8682082ef57e95
Ilja leyberman <7ilya@gmx.de>
2021-05-08 16:07:48 +0200

    Delete src directory

--- src/app/state/app.state.service.ts
+++
@@ -1,7 +0,0 @@
-import {Injectable} from '@angular/core';
-import {StateProperty} from './state.property';
-
-@Injectable({providedIn: 'root'})
-export class AppStateService {
-  simulationRunning = new StateProperty<boolean>(false);
-}
